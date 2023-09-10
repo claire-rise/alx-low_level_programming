da7d260281b61e04eefc1e08cd975efa11e10062
claire-rise <olutayo_4care.prof@yahoo.com>
2023-09-09 18:28:50 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *
+ *This program print exactly print text using puts() function
+ *Return 0 at the end
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
