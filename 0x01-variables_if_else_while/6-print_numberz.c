adb6f19c08eff6cfb812db5d6b086ffedaa87838
claire-rise <olutayo_4care.prof@yahoo.com>
2023-09-25 01:38:39 +0000

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Program to print digits numbers 0 to 9 using putchar
+ *
+ * Return: return 0 always
+ */
+int main(void)
+{
+	int digitNum;
+
+	for (digitNum = 0; digitNum <= 9; digitNum++)
+	{
+		putchar((char)digitNum);
+	}
+
+	putchar('\n');
+	return (0);
+}
